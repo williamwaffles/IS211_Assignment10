1ab3ade3a8b8723074ede8e6aa99b3f3bb7b780c
williamwaffles <41193361+williamarnaiz@users.noreply.github.com>
2021-11-10 20:13:23 -0500

    Update music.sql
    Added framework for music database

--- music.sql
+++ music.sql
@@ -1,1 +1,20 @@
+CREATE TABLE artists (
+  id INTEGER PRIMARY KEY,
+  artist TEXT,
+);
 
+CREATE TABLE albums (
+  id INTEGER PRIMARY KEY,
+  artist_name TEXT,
+  album_name TEXT,
+);
+
+CREATE TABLE songs (
+  id INTEGER PRIMARY KEY,
+  alb_name TEXT,
+  song_name TEXT,
+  track_number INTEGER,
+  track_length INTEGER
+);
+
+  
